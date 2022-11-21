86299d76646540cafe4e4ebcfbbd9afce2d7f886
Verkin78 <otrajenie.jc@gmail.com>
2022-11-21 20:24:40 +0300

    Setters До написания сеттера всё работало. программа предлагает добавить метод в Book, а он там есть. не понимаю причину ошибки.

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,14 @@ public class Main {
         Auther auther1 = new Auther("Michael", "Ivanov");
         Book book2 = new Book("Weather at Home", "Smith", "1794");
         Auther auther2 = new Auther("Lincy", "Smith");
+        book2.setCreateYear("1790");
         System.out.println(" Название книги: " + book1.getBookName() + ". Автор: " + book1.getBookAuther()
                 + ". Год публикации: " + book1.getCreateYear() + ".");
         System.out.println(" Автор книги: " + auther1.getName() + " " + auther1.getLastName() + ".");
         System.out.println(" Название книги: " + book2.getBookName() + ". Автор: " + book2.getBookAuther()
                 + ". Год публикации: " + book2.getCreateYear() + ".");
         System.out.println(" Автор книги: " + auther2.getName() + " " + auther2.getLastName() + ".");
+
     }
 
 }
