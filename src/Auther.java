05ab9b7670432a6bdfaf4fdc292821c52243977d
Verkin78 <otrajenie.jc@gmail.com>
2022-11-21 17:18:28 +0300

    getters for Book

---
+++ src/Auther.java
@@ -0,0 +1,9 @@
+public class Auther {
+    String name;
+    String lastName;
+
+    public Auther (String name,String lastName){
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
---
+++ src/Book.java
@@ -0,0 +1,20 @@
+public class Book {
+    String bookName;
+    String augther;
+    int createYear;
+
+    public Book(String bookName, String augther, int createYear) {
+        this.bookName = bookName;
+        this.augther = augther;
+        this.createYear = createYear;
+    }
+            public String getBookName() {
+        return bookName;
+    }
+    public String getAugther() {
+        return augther;
+    }
+    public int getCreateYear() {
+        return createYear;
+    }
+}
